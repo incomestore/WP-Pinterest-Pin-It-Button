ecbfa9113853155dc809417dba048023817d4cb4
Phil Derksen <pderksen@gmail.com>
2013-05-16 11:52:29 -0700

    Starting with plugin boilerplate files
    From tommcfarlin / WordPress-Plugin-Boilerplate

---
+++ pinterest-pin-it-button/uninstall.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package  PluginName
+ * @author   TODO
+ * @license  GPL-2.0+
+ * @link     TODO
+ * @version  1.0.0
+ * @since    1.0.0
+ */
+
+// If uninstall, not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// TODO: Define uninstall functionality here
---
+++ pinterest-pin-it-button/views/admin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package    PluginName
+ * @author     TODO
+ * @license    GPL-2.0+
+ * @link       TODO
+ * @since      1.0.0
+ */
+?>
+<div class="wrap">
+
+	<?php screen_icon(); ?>
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+	<!-- TODO: Provide markup for your options page here. -->
+
+</div>
