6a552b66639cc66ee9a56168b7f35b83b306ec1b
Nick Young <mycorpweb@gmail.com>
2013-06-28 11:50:58 -0600

    removed some files that were not supposed to be moved here.

--- pinterest-pin-it-button/js/gplus-embed.js
+++
@@ -1,8 +0,0 @@
-//Embed JS that gets rendered in footer once if using official Google +1 JS buttons
-//https://developers.google.com/+/plugins/+1button/
-
-(function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
