66637167e824f3b7ed44bda1a771062137b438c9
Phil Derksen <pderksen@gmail.com>
2013-07-02 13:19:02 -0700

    Removed unused JS files

--- pinterest-pin-it-button/js/pin-it-button-image-selected.js
+++
@@ -1,8 +0,0 @@
-//Add click event for image-selected/no-iframe version
-jQuery(document).ready(function($) {
-    $("a.pin-it-button-image-selected").click(function(event) {
-        event.preventDefault();
-        var modal = window.open($(this).attr('href'),'pibModal','width=665,height=300');
-        if (window.focus) { modal.focus(); }
-    });
-});
