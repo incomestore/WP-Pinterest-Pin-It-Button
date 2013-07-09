6d41d28a3f52a869e1a539f5157b6d4912bee0d1
Nick Young <mycorpweb@gmail.com>
2013-07-09 13:45:11 -0600

    Changed wording for custom update messages

--- pinterest-pin-it-button/views/notices.php
+++ pinterest-pin-it-button/views/notices.php
@@ -13,19 +13,19 @@ function pib_register_admin_notices() {
 	global $pib_options;
 	
 	if( ( isset( $_GET['tab'] ) && 'general' == $_GET['tab'] ) && ( isset( $_GET['settings-updated'] ) && 'true' == $_GET['settings-updated'] ) ) {
-		add_settings_error( 'pib-notices', 'pib-styles', __( 'General options updated', 'pib' ), 'updated' );
+		add_settings_error( 'pib-notices', 'pib-styles', __( 'General settings updated.', 'pib' ), 'updated' );
 	}
 	
 	if( ( isset( $_GET['tab'] ) && 'post_visibility' == $_GET['tab'] ) && ( isset( $_GET['settings-updated'] ) && 'true' == $_GET['settings-updated'] ) ) {
-		add_settings_error( 'pib-notices', 'pib-styles', __( 'Post Visibility options updated', 'pib' ), 'updated' );
+		add_settings_error( 'pib-notices', 'pib-styles', __( 'Post Visibility settings updated.', 'pib' ), 'updated' );
 	}
 	
 	if( ( isset( $_GET['tab'] ) && 'styles' == $_GET['tab'] ) && ( isset( $_GET['settings-updated'] ) && 'true' == $_GET['settings-updated'] ) ) {
-		add_settings_error( 'pib-notices', 'pib-styles', __( 'Styles options updated', 'pib' ), 'updated' );
+		add_settings_error( 'pib-notices', 'pib-styles', __( 'Styles settings updated.', 'pib' ), 'updated' );
 	}
 	
 	if( ( isset( $_GET['tab'] ) && 'misc' == $_GET['tab'] ) && ( isset( $_GET['settings-updated'] ) && 'true' == $_GET['settings-updated'] ) ) {
-		add_settings_error( 'pib-notices', 'pib-styles', __( 'Misc options updated', 'pib' ), 'updated' );
+		add_settings_error( 'pib-notices', 'pib-styles', __( 'Misc settings updated.', 'pib' ), 'updated' );
 	}
 	
 	settings_errors( 'pib-notices' );
