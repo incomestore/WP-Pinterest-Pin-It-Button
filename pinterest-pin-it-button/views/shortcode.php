479b5b7e0d9404b63c4fa5230c26fa449240b67d
Nick Young <mycorpweb@gmail.com>
2013-07-10 23:45:46 -0600

    Restructured the way shortcode.php works. Also removed any references to the pro feature of social_buttons

--- pinterest-pin-it-button/views/shortcode.php
+++ pinterest-pin-it-button/views/shortcode.php
@@ -11,17 +11,22 @@ function pib_button_shortcode_html( $attr ) {
         2) custom fields for post
         3) inherit from post: permalink, first image, post title
     */
-
-    $url = $attr['url'];
     
+    extract( shortcode_atts( array(
+					'count' => 'none',
+					'url' => '',
+					'image_url' => '',
+					'description' => '',
+					'float' => 'none',
+					'remove_div' => false
+				), $attr ) );
+
     if ( empty( $url ) ) {
         $url = get_post_meta( $postID, 'pib_url_of_webpage', true);
         if ( empty( $url ) ) {
             $url = get_permalink( $postID );
         }
     }
-    
-    $image_url = $attr['image_url'];
     
     if ( empty( $image_url ) ) {
         $image_url = get_post_meta( $postID, 'pib_url_of_img', true);
@@ -33,33 +38,27 @@ function pib_button_shortcode_html( $attr ) {
         }
     }
     
-    $description = $attr['description'];
-    
     if ( empty( $description ) ) {
         $description = get_post_meta( $postID, 'pib_description', true);
         if ( empty( $description ) ) {
             $description = get_the_title( $postID );
         }
     }
-        
-	$count = ( empty( $attr['count'] ) ? 'none' : $attr['count'] );
-	$remove_div = ( $attr['remove_div'] == 'true' );
-    $social_buttons = ( $attr['social_buttons'] == 'true' );
     
 	$base_btn = pib_button_base( $url, $image_url, $description, $count );
     
-    //Don't wrap with div or use float left/right if using other sharing buttons or "remove div" is checked
-	if ( $remove_div || $social_buttons ) {
+    //Don't wrap with div or use float left/right if "remove div" is checked
+	if ( $remove_div ) {
 		return $base_btn;
 	}
 	else {
 		//Surround with div tag
 		$float_class = '';
 		
-		if ( $attr['float'] == 'left' ) {
+		if ( $float == 'left' ) {
 			$float_class = 'pib-float-left';
 		}
-		elseif ( $attr['float'] == 'right' ) {
+		elseif ( $float == 'right' ) {
 			$float_class = 'pib-float-right';
 		}
 	
