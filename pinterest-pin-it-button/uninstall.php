04254157149b4b3dc640e7e1ed8b5f9a7cc82e92
Phil Derksen <pderksen@gmail.com>
2013-07-15 22:28:05 -0700

    Updated comments.

--- pinterest-pin-it-button/uninstall.php
+++ pinterest-pin-it-button/uninstall.php
@@ -7,18 +7,17 @@
  * @author  Phil Derksen <pderksen@gmail.com>, Nick Young <mycorpweb@gmail.com>
  */
 
-// If uninstall, not called from WordPress, then exit
+// If uninstall not called from WordPress, then exit
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-// TODO: Define uninstall functionality here
 $general = get_option( 'pib_settings_general' );
 
 // If the the option to save settings is checked then do nothing, otherwise delete all options and post meta
-if( $general['uninstall_save_settings'] ) { } 
-else {
-	
+if( $general['uninstall_save_settings'] ) {
+	// Do nothing
+} else {
 	// Delete options
 	delete_option( 'pib_settings_general' );
 	delete_option( 'pib_settings_post_visibility' );
