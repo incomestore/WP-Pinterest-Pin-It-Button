f7015abd8ac15888fe83d3ebc186fc25e1bcb616
Phil Derksen <pderksen@gmail.com>
2013-07-16 16:11:05 -0700

    Widget settings label and text changes.

--- pinterest-pin-it-button/views/widget.php
+++ pinterest-pin-it-button/views/widget.php
@@ -14,9 +14,9 @@
 class PIB_Widget extends WP_Widget {
 	
 	function __construct() {
-		$widget_ops = array( 'classname' => 'pib-clearfix', 'description' => __( 'Add a Pinterest "Pin It" button to your sidebar with this widget.' ) );
+		$widget_ops = array( 'classname' => 'pib-clearfix', 'description' => __( 'Add a Pinterest "Pin It" button to your sidebar with this widget.', 'pib' ) );
 		$control_ops = array( 'width' => 400 );  //doesn't use height
-		parent::__construct( 'pib_button', __( 'Pinterest "Pin It" Button' ), $widget_ops, $control_ops );
+		parent::__construct( 'pib_button', 'Pinterest "Pin It" Button', $widget_ops, $control_ops );
 	}
 
 	function widget( $args, $instance ) {
@@ -85,66 +85,67 @@ class PIB_Widget extends WP_Widget {
         global $pib_options;
         
 		$instance = wp_parse_args( (array) $instance, array( 'count_layout' => 'none', 'title' => '', 
-		'pib_count_button_radio' => 'user_selects_image', 'float' => 'none' ) );
+			'pib_count_button_radio' => 'user_selects_image', 'float' => 'none' ) );
 		$title = strip_tags($instance['title']);
 		$pib_url_of_webpage_widget = strip_tags( $instance['pib_url_of_webpage_widget'] );
 		$pib_url_of_img_widget = strip_tags( $instance['pib_url_of_img_widget'] );
 		$pib_description_widget = strip_tags( $instance['pib_description_widget'] );
-		$pib_button_style_widget = ( $pib_options['button_style'] == 'user_selects_image' ) ? 'User selects image' : 'Image pre-selected';		
+		$pib_button_style_widget = ( $pib_options['button_style'] == 'user_selects_image' ) ? __( 'User selects image', 'pib' ) : __( 'Image pre-selected', 'pib' );
 		?>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title (optional):' ); ?></label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title (optional):', 'pib' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'count_layout' ); ?>"><?php _e( 'Pin Count:' ); ?></label> 
+			<label for="<?php echo $this->get_field_id( 'count_layout' ); ?>"><?php _e( 'Pin Count:', 'pib' ); ?></label>
 			<select name="<?php echo $this->get_field_name( 'count_layout' ); ?>" id="<?php echo $this->get_field_id( 'count_layout' ); ?>">
-				<option value="none" <?php selected( $instance['count_layout'], 'none' ); ?>><?php _e( 'No Count'); ?></option>
-				<option value="horizontal" <?php selected( $instance['count_layout'], 'horizontal' ); ?>><?php _e( 'Horizontal' ); ?></option>
-				<option value="vertical" <?php selected( $instance['count_layout'], 'vertical' ); ?>><?php _e( 'Vertical' ); ?></option>
+				<option value="none" <?php selected( $instance['count_layout'], 'none' ); ?>><?php _e( 'Not Shown', 'pib' ); ?></option>
+				<option value="horizontal" <?php selected( $instance['count_layout'], 'horizontal' ); ?>><?php _e( 'Beside the Button', 'pib' ); ?></option>
+				<option value="vertical" <?php selected( $instance['count_layout'], 'vertical' ); ?>><?php _e( 'Above the Button', 'pib' ); ?></option>
 			</select>
 		</p>
 		<div class="pib-widget-text-fields">
             <p>
-                <?php _e( 'Button style is inherited from setting saved in' ); ?>
-                    <a href="<?php echo admin_url( 'admin.php?page=pinterest-pin-it-button' ); ?>"><?php _e( '"Pin It" Button Settings' ); ?></a>
+                <?php _e( 'Button style is inherited from setting saved in', 'pib' ); ?>
+                    <a href="<?php echo admin_url( 'admin.php?page=pinterest-pin-it-button' ); ?>"><?php _e( '"Pin It" Button Settings', 'pib' ); ?></a>.
             </p>
             <p>
-				<?php _e( 'Current style' ) ?>: <strong><?php echo $pib_button_style_widget; ?></strong>
+				<?php _e( 'Current style', 'pib' ) ?>: <strong><?php echo $pib_button_style_widget; ?></strong>
             </p>
 			<p>
-				<label for="<?php echo $this->get_field_id( 'pib_url_of_webpage_widget' ); ?>"><?php _e( 'URL of the web page to be pinned' ); ?></label>
+				<label for="<?php echo $this->get_field_id( 'pib_url_of_webpage_widget' ); ?>"><?php _e( 'URL of the web page to be pinned', 'pib' ); ?></label>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'pib_url_of_webpage_widget' ); ?>" name="<?php echo $this->get_field_name( 'pib_url_of_webpage_widget' ); ?>"
 					   type="text" value="<?php echo esc_attr( $pib_url_of_webpage_widget ); ?>" />
-                <span class="description"><?php _e( 'Required for "image pre-selected". Defaults to home page for "user selects image".'); ?></span>
+                <span class="description"><?php _e( 'Required for "image pre-selected". Defaults to home page for "user selects image".', 'pib' ); ?></span>
 			</p>
 			<p>
-				<label for="<?php echo $this->get_field_id( 'pib_url_of_img_widget' ); ?>"><?php _e( 'URL of the image to be pinned' ); ?></label>
+				<label for="<?php echo $this->get_field_id( 'pib_url_of_img_widget' ); ?>"><?php _e( 'URL of the image to be pinned', 'pib' ); ?></label>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'pib_url_of_img_widget' ); ?>" name="<?php echo $this->get_field_name( 'pib_url_of_img_widget' ); ?>"
 					   type="text" value="<?php echo esc_attr( $pib_url_of_img_widget ); ?>" />
-                <span class="description"><?php _e( 'Required for "image pre-selected". Not used for "user selects image".' ); ?></span>
+                <span class="description"><?php _e( 'Required for "image pre-selected". Not used for "user selects image".', 'pib' ); ?></span>
 			</p>
 			<p>
-				<label for="<?php echo $this->get_field_id( 'pib_description_widget' ); ?>"><?php _e( 'Description of the pin (optional)' ); ?></label>
+				<label for="<?php echo $this->get_field_id( 'pib_description_widget' ); ?>"><?php _e( 'Description of the pin (optional)', 'pib' ); ?></label>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'pib_description_widget' ); ?>" name="<?php echo $this->get_field_name( 'pib_description_widget' ); ?>"
 					   type="text" value="<?php echo esc_attr( $pib_description_widget ); ?>" />
 			</p>
 		</div>
 		
 		<p>
-			<label for="<?php echo $this->get_field_id( 'float' ); ?>"><?php _e( 'Align (float)' ); ?>:</label>
+			<label for="<?php echo $this->get_field_id( 'float' ); ?>"><?php _e( 'Alignment', 'pib' ); ?>:</label>
 			<select name="<?php echo $this->get_field_name( 'float' ); ?>" id="<?php echo $this->get_field_id( 'float' ); ?>">
-				<option value="none" <?php selected( $instance['float'], 'none' ); ?>><?php _e( 'none (default)' ); ?></option>
-				<option value="left" <?php selected( $instance['float'], 'left' ); ?>><?php _e( 'left' ); ?></option>
-				<option value="right" <?php selected( $instance['float'], 'right' ); ?>><?php _e( 'right' ); ?></option>
+				<option value="none" <?php selected( $instance['float'], 'none' ); ?>><?php _e( 'None', 'pib' ); ?></option>
+				<option value="left" <?php selected( $instance['float'], 'left' ); ?>><?php _e( 'Left', 'pib' ); ?></option>
+				<option value="right" <?php selected( $instance['float'], 'right' ); ?>><?php _e( 'Right', 'pib' ); ?></option>
+				<option value="center" <?php selected( $instance['float'], 'center' ); ?>><?php _e( 'Center', 'pib' ); ?></option>
 			</select>
 		</p>
 		<p>
 			<input class="checkbox" <?php checked($instance['remove_div'], true) ?> id="<?php echo $this->get_field_id( 'remove_div' ); ?>"
 				   name="<?php echo $this->get_field_name( 'remove_div' ); ?>" type="checkbox"/>
 			<label for="<?php echo $this->get_field_id( 'remove_div' ); ?>">
-				<?php _e( 'Remove div tag surrounding this widget button (also removes <strong>float</strong> setting)' ); ?>
+				<?php _e( 'Remove div tag surrounding this widget button. Also removes alignment setting.', 'pib' ); ?>
 			</label>
 		</p>
         <?php
