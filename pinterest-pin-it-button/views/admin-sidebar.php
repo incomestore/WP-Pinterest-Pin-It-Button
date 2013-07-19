e059fb14ac13e74e9e5fd6f288313abfe926d82c
Phil Derksen <pderksen@gmail.com>
2013-07-18 17:01:09 -0700

    Flat UI checkbox glyph from custom font now working.
    Also trying outlined box like Flat UI tagsinput class.

--- pinterest-pin-it-button/views/admin-sidebar.php
+++ pinterest-pin-it-button/views/admin-sidebar.php
@@ -9,6 +9,15 @@
  */
 ?>
 
-<p>Just a button:</p>
-
 <a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me!</a>
+
+<div class="outlined-box">
+	<ul>
+		<li><i class="fui-check"></i> 1st item</li>
+		<li><i class="fui-check"></i> 2nd item</li>
+		<li><i class="fui-check"></i> 3rd item</li>
+	</ul>
+</div>
+
+<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me Again</a>
+
