f9d84d86fa6a38015682de2c5491322d70c46ab3
Phil Derksen <pderksen@gmail.com>
2013-07-18 21:07:17 -0700

    Sidebar marketing text and styles update.

--- pinterest-pin-it-button/views/admin-sidebar.php
+++ pinterest-pin-it-button/views/admin-sidebar.php
@@ -9,26 +9,22 @@
  */
 ?>
 
-<p>
-	<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me!</a>
-</p>
-
 <div class="sidebar-container">
+	<h3>Need More?</h3>
+
 	<ul>
-		<li><i class="fui-check"></i> 1st item</li>
-		<li><i class="fui-check"></i> 2nd item</li>
-		<li><i class="fui-check"></i> 3rd item</li>
+		<li><i class="fui-check"></i> Hover "Pin It" button option</li>
+		<li><i class="fui-check"></i> 30 custom "Pin It" button designs</li>
+		<li><i class="fui-check"></i> Twitter, Facebook & G+ buttons</li>
+		<li><i class="fui-check"></i> Use with featured images</li>
+		<li><i class="fui-check"></i> Use with custom post types</li>
+		<li><i class="fui-check"></i> Upload your button designs</li>
+		<li><i class="fui-check"></i> ...and much more</li>
 	</ul>
 
-	<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me Again</a>
-</div>
+	<p class="last-blurb">
+		Get all of these and more with Pinterest "Pin It" Button Pro!
+	</p>
 
-<div class="outlined-box">
-	<ul>
-		<li><i class="fui-check"></i> 1st item</li>
-		<li><i class="fui-check"></i> 2nd item</li>
-		<li><i class="fui-check"></i> 3rd item</li>
-	</ul>
+	<a href="#fakelink" class="btn btn-large btn-block btn-primary">Upgrade to Pro</a>
 </div>
-
-<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me Again</a>
