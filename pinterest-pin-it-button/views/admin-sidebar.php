de8dceb7e7995d9daa09d040b02cf6ab9de3725f
Phil Derksen <pderksen@gmail.com>
2013-07-18 17:09:50 -0700

    Added lightly shaded Flat UI sidebar container sample...
    to compare with outlined box container.

--- pinterest-pin-it-button/views/admin-sidebar.php
+++ pinterest-pin-it-button/views/admin-sidebar.php
@@ -9,7 +9,19 @@
  */
 ?>
 
-<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me!</a>
+<p>
+	<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me!</a>
+</p>
+
+<div class="sidebar-container">
+	<ul>
+		<li><i class="fui-check"></i> 1st item</li>
+		<li><i class="fui-check"></i> 2nd item</li>
+		<li><i class="fui-check"></i> 3rd item</li>
+	</ul>
+
+	<a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me Again</a>
+</div>
 
 <div class="outlined-box">
 	<ul>
@@ -20,4 +32,3 @@
 </div>
 
 <a href="#fakelink" class="btn btn-large btn-block btn-primary">Click Me Again</a>
-
