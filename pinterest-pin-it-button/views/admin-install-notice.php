68f3584acfdb3211653990238b5fd1a78c0bd03c
Phil Derksen <pderksen@gmail.com>
2013-07-21 16:48:18 -0700

    Using "hide this" button instead of small "x" close button.

--- pinterest-pin-it-button/views/admin-install-notice.php
+++ pinterest-pin-it-button/views/admin-install-notice.php
@@ -11,5 +11,5 @@
 <div id="pib-install-notice" class="alert alert-error">
 	<?php echo PIB_PLUGIN_TITLE . __( ' is now installed.', 'pib' ); ?>
 	<a href="#" class="btn btn-small btn-danger btn-wide"><?php _e( 'Go to Button Setup', 'pib' ); ?></a>
-	<button type="button" class="close fui-cross" data-dismiss="alert"></button>
+	<a href="#" class="btn btn-small"><?php _e( 'Hide this', 'pib' ); ?></a>
 </div>
