8db9a6dbb7e5ac783bf11e022d90ede8cc21d1f4
Phil Derksen <pderksen@gmail.com>
2013-07-23 00:03:21 -0700

    Trimmed down install notice CSS.

--- pinterest-pin-it-button/views/admin-install-notice.php
+++ pinterest-pin-it-button/views/admin-install-notice.php
@@ -14,10 +14,10 @@ if ( ! defined( 'ABSPATH' ) )
 
 ?>
 
-<div id="pib-install-notice">
-	<div class="alert alert-error">
-		<?php echo $this->get_plugin_title() . __( ' is now installed.', 'pib' ); ?>
-		<a href="<?php echo add_query_arg( 'page', $this->plugin_slug, admin_url( 'admin.php' ) ); ?>" class="btn btn-small btn-danger btn-wide"><?php _e( 'Go to Button Setup', 'pib' ); ?></a>
-		<a href="#" class="btn btn-small"><?php _e( 'Hide this', 'pib' ); ?></a>
-	</div>
+<div id="pib-install-notice" class="updated">
+	<p>
+		<span class="pib-installed-text"><?php echo $this->get_plugin_title() . __( ' is now installed.', 'pib' ); ?></span>
+		<a href="<?php echo add_query_arg( 'page', $this->plugin_slug, admin_url( 'admin.php' ) ); ?>" class="button-primary"><?php _e( 'Go to Button Setup', 'pib' ); ?></a>
+		<a href="#" class="button-secondary"><?php _e( 'Hide this', 'pib' ); ?></a>
+	</p>
 </div>
