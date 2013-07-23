0ccab2138485a78e9fb4580f6bd594d5582d56b8
Phil Derksen <pderksen@gmail.com>
2013-07-23 12:29:58 -0700

    Admin sidebar updates.

--- pinterest-pin-it-button/views/admin-sidebar.php
+++ pinterest-pin-it-button/views/admin-sidebar.php
@@ -14,22 +14,24 @@ if ( ! defined( 'ABSPATH' ) )
 ?>
 
 <div class="sidebar-container">
-	<h3><?php _e( 'Need More Options?', 'pib' ); ?></h3>
+	<h3 class="sidebar-title"><?php _e( 'Need More Options?', 'pib' ); ?></h3>
 
-	<ul>
-		<li><i class="fui-check"></i> <?php _e( 'Image Hover "Pin It" button', 'pib' ); ?></li>
-		<li><i class="fui-check"></i> <?php _e( '30 custom "Pin It" button designs', 'pib' ); ?></li>
-		<li><i class="fui-check"></i> <?php _e( 'Twitter, Facebook & G+ buttons', 'pib' ); ?></li>
-		<li><i class="fui-check"></i> <?php _e( 'Featured image support', 'pib' ); ?></li>
-		<li><i class="fui-check"></i> <?php _e( 'Custom post type support', 'pib' ); ?></li>
-		<li><i class="fui-check"></i> <?php _e( 'Upload your own button designs', 'pib' ); ?></li>
-		<li><i class="fui-check"></i> <?php _e( 'Priority support & auto updates', 'pib' ); ?></li>
-	</ul>
+	<div class="sidebar-content">
+		<ul>
+			<li><i class="fui-check"></i> <?php _e( 'Image Hover "Pin It" button', 'pib' ); ?></li>
+			<li><i class="fui-check"></i> <?php _e( '30 custom "Pin It" button designs', 'pib' ); ?></li>
+			<li><i class="fui-check"></i> <?php _e( 'Twitter, Facebook & G+ buttons', 'pib' ); ?></li>
+			<li><i class="fui-check"></i> <?php _e( 'Featured image support', 'pib' ); ?></li>
+			<li><i class="fui-check"></i> <?php _e( 'Custom post type support', 'pib' ); ?></li>
+			<li><i class="fui-check"></i> <?php _e( 'Upload your own button designs', 'pib' ); ?></li>
+			<li><i class="fui-check"></i> <?php _e( 'Priority support & auto updates', 'pib' ); ?></li>
+		</ul>
 
-	<p class="last-blurb">
-		<?php _e( 'Get all of these and more with Pinterest "Pin It" Button Pro!', 'pib' ); ?>
-	</p>
+		<p class="last-blurb">
+			<?php _e( 'Get all of these and more with Pinterest "Pin It" Button Pro!', 'pib' ); ?>
+		</p>
 
-	<a href="<?php echo pib_pro_upgrade_url( 'sidebar_link' ); ?>" class="btn btn-large btn-block btn-danger" target="_blank">
-		<?php _e( 'Upgrade to Pro Now', 'pib' ); ?></a>
+		<a href="<?php echo pib_pro_upgrade_url( 'sidebar_link' ); ?>" class="btn btn-large btn-block btn-danger" target="_blank">
+			<?php _e( 'Upgrade to Pro Now', 'pib' ); ?></a>
+	</div>
 </div>
