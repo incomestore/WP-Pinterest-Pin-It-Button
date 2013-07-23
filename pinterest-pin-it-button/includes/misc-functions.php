0394521f10bff0ff20c442786dcf7a6b0c4b4ea9
Phil Derksen <pderksen@gmail.com>
2013-07-23 13:39:20 -0700

    Admin sidebar updates.
    - More Flat UI styles
    - Rate on WordPress call-to-action
    - RSS links from pinterestplugin.com shown

--- pinterest-pin-it-button/includes/misc-functions.php
+++ pinterest-pin-it-button/includes/misc-functions.php
@@ -33,3 +33,46 @@ function pib_pro_upgrade_url( $medium ) {
 
 	return $url;
 }
+
+// Render RSS items from pinterestplugin.com in unordered list.
+// http://codex.wordpress.org/Function_Reference/fetch_feed
+
+function pib_rss_news() {
+	// Get RSS Feed(s)
+	include_once( ABSPATH . WPINC . '/feed.php' );
+
+	// Get a SimplePie feed object from the specified feed source.
+	$rss = fetch_feed( 'http://pinterestplugin.com/feed/' );
+
+	if ( ! is_wp_error( $rss ) ) {
+		// Checks that the object is created correctly
+		// Figure out how many total items there are, but limit it to 5.
+		$maxitems = $rss->get_item_quantity( 3 );
+
+		// Build an array of all the items, starting with element 0 (first element).
+		$rss_items = $rss->get_items( 0, $maxitems );
+	}
+	?>
+
+	<ul>
+		<?php if ($maxitems == 0): ?>
+			<li><?php _e( 'No items.', 'pib' ); ?></li>
+		<?php else: ?>
+			<?php
+			// Loop through each feed item and display each item as a hyperlink.
+			foreach ( $rss_items as $item ): ?>
+				<?php $post_url = add_query_arg( array(
+									'utm_source'   => 'pib_lite',
+									'utm_medium'   => 'sidebar_link',
+									'utm_campaign' => 'blog_post_link'
+								), esc_url( $item->get_permalink() ) ); ?>
+
+				<li>
+					&raquo; <a href="<?php echo $post_url; ?>" target="_blank" class="pib-external-link"><?php echo esc_html( $item->get_title() ); ?></a>
+				</li>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</ul>
+
+	<?php
+}
--- pinterest-pin-it-button/views/admin-sidebar.php
+++ pinterest-pin-it-button/views/admin-sidebar.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) )
 ?>
 
 <div class="sidebar-container">
-	<h3 class="sidebar-title"><?php _e( 'Need More Options?', 'pib' ); ?></h3>
+	<h3 class="sidebar-title-large"><?php _e( 'Need More Options?', 'pib' ); ?></h3>
 
 	<div class="sidebar-content">
 		<ul>
@@ -33,5 +33,24 @@ if ( ! defined( 'ABSPATH' ) )
 
 		<a href="<?php echo pib_pro_upgrade_url( 'sidebar_link' ); ?>" class="btn btn-large btn-block btn-danger" target="_blank">
 			<?php _e( 'Upgrade to Pro Now', 'pib' ); ?></a>
+	</div>
+</div>
+
+<div class="sidebar-container">
+	<div class="sidebar-content">
+		<p class="rating-text">
+			<?php _e( "Help us get noticed (and boost our egos) with a rating and short review.", 'pib' ); ?>
+		</p>
+
+		<a href="http://wordpress.org/support/view/plugin-reviews/pinterest-pin-it-button" class="btn btn-small btn-block btn-inverse" target="_blank">
+			<?php _e( 'Rate this plugin on WordPress.org', 'pib' ); ?></a>
+	</div>
+</div>
+
+<div class="sidebar-container">
+	<h4 class="sidebar-title-small"><?php _e( 'Recent News from PinterestPlugin.com', 'pib' ); ?></h4>
+
+	<div class="sidebar-content pib-rss-news">
+		<?php pib_rss_news(); ?>
 	</div>
 </div>
