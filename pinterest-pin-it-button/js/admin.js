0c57d3827faf73393480fe041f5429228048cebb
Phil Derksen <pderksen@gmail.com>
2013-07-25 15:51:25 -0700

    Removed attempt at widget admin toggle jQuery.

--- pinterest-pin-it-button/js/admin.js
+++ pinterest-pin-it-button/js/admin.js
@@ -1,31 +1,6 @@
 (function ($) {
 	"use strict";
 	$(function () {
-		//Administration-specific JavaScript
-		
-		// Show/Hide the input boxes for the widget
-		function toggleWidgetArea() {
-			
-			$( "input.pib-widget-pre-selected" ).each( function() {
-				if( $( this ).prop( "checked" ) ) {
-					$( this ).parent().next( ".pib-widget-text-fields" ).show();
-				}
-			});
-			
-			
-			$( "input.pib-widget-toggle" ).change( function() {
-				$( this ).parent().parent().find( ".pib-widget-text-fields" ).toggle();
-			});
-		}
-		
-		$(document).ajaxComplete( function() {
-			// We need to do this since the widget saves using AJAX
-			toggleWidgetArea();
-		});
-		
-		$(document).ready( function() {
-			// Call our widget area code
-			toggleWidgetArea();
-		});
+		// Administration-specific JavaScript.
 	});
 }(jQuery));
