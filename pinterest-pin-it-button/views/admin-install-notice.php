b9fabb863ad953dd81e9e85b7982dcb2ab1f5f94
Phil Derksen <pderksen@gmail.com>
2013-07-24 21:55:06 -0700

    Install notice verbage update.

--- pinterest-pin-it-button/views/admin-install-notice.php
+++ pinterest-pin-it-button/views/admin-install-notice.php
@@ -23,8 +23,8 @@ if ( ! defined( 'ABSPATH' ) )
 
 <div id="pib-install-notice" class="updated">
 	<p>
-		<?php echo $this->get_plugin_title() . __( ' is now installed and activated!', 'pib' ); ?>
-		<a href="<?php echo add_query_arg( 'page', $this->plugin_slug, admin_url( 'admin.php' ) ); ?>" class="button-primary"><?php _e( 'Go to Button Setup', 'pib' ); ?></a>
+		<?php echo $this->get_plugin_title() . __( ' is now installed.', 'pib' ); ?>
+		<a href="<?php echo add_query_arg( 'page', $this->plugin_slug, admin_url( 'admin.php' ) ); ?>" class="button-primary"><?php _e( 'Setup your Pin It button now', 'pib' ); ?></a>
 		<a href="<?php echo add_query_arg( 'pib-dismiss-install-nag', 1 ); ?>" class="button-secondary"><?php _e( 'Hide this', 'pib' ); ?></a>
 	</p>
 </div>
