2c58cfe4782d3523dbb9e49d9801b81c1837c441
Phil Derksen <pderksen@gmail.com>
2013-09-24 21:02:08 -0700

    Removed unused deactivation hook.

--- pinterest-pin-it-button/pinterest-pin-it-button.php
+++ pinterest-pin-it-button/pinterest-pin-it-button.php
@@ -28,6 +28,5 @@ if( ! class_exists( 'Pinterest_Pin_It_Button' ) )
 
 // Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
 register_activation_hook( __FILE__, array( 'Pinterest_Pin_It_Button', 'activate' ) );
-//register_deactivation_hook( __FILE__, array( 'Pinterest_Pin_It_Button', 'deactivate' ) );
 
 Pinterest_Pin_It_Button::get_instance();
